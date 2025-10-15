8ecf69f5b593a9b3af9baacf400ccce29fc571aa
masooma zahedi <masooma.zahedi1@gmail.com>
2025-10-14 20:22:55 -0500

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,13 +6,13 @@ import SpellingGame from "../sections/alphabet/SpellingGame";
 export default function Home() {
   return (<>
   <div >
-    <div className="h5 m-5 border border-3 border-warning p-4 text-center bg-info rounded">لطفا بچه ها داستان را تا بالای عکس بخوانند و بنویسند . همین طور فعالیت وسایل مدرسه را انجام دهند .این برای نوشتن آنها خوب است. </div>
+    <div className="h5 m-5 border border-3 border-warning p-4 text-center bg-info rounded">لطفا بچه ها قسمت پایین عکس داستان را به همراه کلمات و معنی آن بنویسند. </div>
+    <SpellingGame/>
     <StoryPage3 groupKey="alefba" wantedTitle="خارپُشت وَ پاییز"/>
     {/* <WordGameWithCategories initialCategory="وسایل مدرسه" /> */}
-    <SpellingGame/>
     <div>
       <h5 className="bg-warning p-4 m-4 rounded text-center">برای احسان </h5>
-          <StoryPage3 groupKey="kalemat" wantedTitle="باغِ وَحش"/>
+          <StoryPage3 groupKey="kalemat" wantedTitle="پیک نیک"/>
     </div>
 
 
