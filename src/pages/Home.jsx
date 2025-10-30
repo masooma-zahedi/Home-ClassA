3a596f784bf9d02949b308a90725de3d4071f5a7
masooma zahedi <masooma.zahedi1@gmail.com>
2025-10-29 19:16:51 -0500

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,21 +5,23 @@ import SpellingGame from "../sections/alphabet/SpellingGame";
 import DragDropQuiz from "../sections/word/DragDropQuiz";
 import SortableSentencesApp from "../sections/word/SortableSentencesApp";
 import SentenceObject from "../sections/word/SentenceObject";
+import ObjectSentenceGame from "../sections/alphabet/ObjectSentenceGame";
 
 export default function Home() {
   return (<>
   <div >
-    <div className="h5 m-5 border border-3 border-warning p-4 text-center bg-info rounded">لطفا بچه ها از روی 5 جمله به دلخواه بنویسند و برای شما بخوانند. . </div>
+    <div className="h5 m-5 border border-3 border-warning p-4 text-center bg-info rounded">بچه ها جملات زیر را تمرین کنند و از روی 7 جمله جواب درست آن بنویسند.  </div>
     {/* <SpellingGame/> */}
     {/* <WordGameWithCategories initialCategory="وسایل مدرسه" /> */}
-    <SentenceObject/>
-        <StoryPage3 groupKey="alefba" wantedTitle="روباه وَ رُودخانه"/>
+    {/* <SentenceObject/> */}
+        {/* <StoryPage3 groupKey="alefba" wantedTitle="روباه وَ رُودخانه"/> */}
         {/* <DragDropQuiz/> */}
+        <ObjectSentenceGame/>
 
     <div>
-      <h5 className="bg-warning p-4 m-4 rounded text-center">برای احسان : جملات زیر را بر اساس زمان اتفاق مرتب کند.  </h5>
-          <SortableSentencesApp/>
-          {/* <StoryPage3 groupKey="kalemat" wantedTitle="پیک نیک"/> */}
+      <h5 className="bg-warning p-4 m-4 rounded text-center">برای احسان : خلاصه داستان را بنویسید.  </h5>
+          {/* <SortableSentencesApp/> */}
+          <StoryPage3 groupKey="kalemat" wantedTitle="مینا و راستگویی"/>
     </div>
   </div>
 
