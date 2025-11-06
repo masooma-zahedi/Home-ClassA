70b424bdcd6424aa36d3c839aeb9a7957847c754
masooma zahedi <masooma.zahedi1@gmail.com>
2025-11-05 19:13:09 -0600

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,22 +6,32 @@ import DragDropQuiz from "../sections/word/DragDropQuiz";
 import SortableSentencesApp from "../sections/word/SortableSentencesApp";
 import SentenceObject from "../sections/word/SentenceObject";
 import ObjectSentenceGame from "../sections/alphabet/ObjectSentenceGame";
+import VideoWithCaptions from "../sections/alphabet/VideoCard";
+import VideoCard from "../sections/alphabet/VideoCard";
+import SentenceBuilder from "../sections/word/SentenceBuilder";
 
 export default function Home() {
   return (<>
   <div >
     <div className="h5 m-5 border border-3 border-warning p-4 text-center bg-info rounded">بچه ها جملات زیر را تمرین کنند و از روی 7 جمله جواب درست آن بنویسند.  </div>
-    {/* <SpellingGame/> */}
     {/* <WordGameWithCategories initialCategory="وسایل مدرسه" /> */}
     {/* <SentenceObject/> */}
         {/* <StoryPage3 groupKey="alefba" wantedTitle="روباه وَ رُودخانه"/> */}
         {/* <DragDropQuiz/> */}
-        <ObjectSentenceGame/>
-
+        {/* <ObjectSentenceGame/> */}
+        {/* <VideoWithCaptions/> */}
+      <VideoCard
+        title="قصه کوتاه امروز"
+        description="متن فارسی داستان زیر را بخوانید."
+        videoFileName={`${process.env.PUBLIC_URL}/video/chick.mp4`}  // فقط اسم فایل ویدیوی mp4
+      />
+      <SpellingGame/>
+{/* ****************************************************** */}
     <div>
       <h5 className="bg-warning p-4 m-4 rounded text-center">برای احسان : خلاصه داستان را بنویسید.  </h5>
           {/* <SortableSentencesApp/> */}
-          <StoryPage3 groupKey="kalemat" wantedTitle="مینا و راستگویی"/>
+          <SentenceBuilder/>
+          {/* <StoryPage3 groupKey="kalemat" wantedTitle="مینا و راستگویی"/> */}
     </div>
   </div>
 
