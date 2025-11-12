c0f9cd0293dcf131df5874ee6d70939cfdf5eec7
masooma zahedi <masooma.zahedi1@gmail.com>
2025-11-11 20:20:53 -0600

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,25 +13,26 @@ import SentenceBuilder from "../sections/word/SentenceBuilder";
 export default function Home() {
   return (<>
   <div >
-    <div className="h5 m-5 border border-3 border-warning p-4 text-center bg-info rounded">بچه ها فیلم زیر را ببینند و جملات فارسی آن را بخوانند. و از روی 6 جمله آن بنویسند.  </div>
+    <div className="h5 m-5 border border-3 border-warning p-4 text-center bg-info rounded">بچه ها داستان زیر را بخوانند و تا بالای عکس بنویسند.  </div>
     {/* <WordGameWithCategories initialCategory="وسایل مدرسه" /> */}
     {/* <SentenceObject/> */}
-        {/* <StoryPage3 groupKey="alefba" wantedTitle="روباه وَ رُودخانه"/> */}
+        <StoryPage3 groupKey="alefba" wantedTitle="اَمـانَـتِ مینا"/>
         {/* <DragDropQuiz/> */}
         {/* <ObjectSentenceGame/> */}
         {/* <VideoWithCaptions/> */}
-      <VideoCard
+      {/* <VideoCard
         title="قصه کوتاه امروز"
         description="متن فارسی داستان زیر را بخوانید."
         videoFileName={`${process.env.PUBLIC_URL}/video/chick.mp4`}  // فقط اسم فایل ویدیوی mp4
-      />
+      /> */}
       <SpellingGame/>
 {/* ****************************************************** */}
     <div>
-      <h5 className="bg-warning p-4 m-4 rounded text-center">برای احسان : جملات بهم ریخته زیر را مرتب و از روی 6 جمله آن بنویسد. .  </h5>
+      <h5 className="bg-warning p-4 m-4 rounded text-center">برای احسان : داستان زیر را بخواند و به سوالات درک مطلب جواب دهد.  </h5>
           {/* <SortableSentencesApp/> */}
-          <SentenceBuilder/>
+          {/* <SentenceBuilder/> */}
           {/* <StoryPage3 groupKey="kalemat" wantedTitle="مینا و راستگویی"/> */}
+          <StoryPage3 groupKey="dastan" wantedTitle="آهوی یک‌چشم *"/>
     </div>
   </div>
 
