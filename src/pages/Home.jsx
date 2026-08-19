21f102be8a59c89be7fbfd8f6c1bcb39b2a9f354
masooma zahedi <masooma.zahedi1@gmail.com>
2026-08-18 20:23:03 -0500

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,8 @@ import ChooseWords from "../sections/alphabet/ChooseWords";
 export default function Home() {
   return (<>
   <div >
-    <div className="h5 m-5 border border-3 border-warning p-4 text-center bg-info rounded">بچه ها : داستان زیر را بخوانند و از روی کلمات  آن دو بار با معنی بنویسند..  </div>
+    <div className="h5 m-5 border border-3 border-warning p-4 text-center bg-info rounded">بچه ها : جملات زیر را بخوانند و از روی 4 جمله آن بنویسند..  </div>
+    {/* <div className="h5 m-5 border border-3 border-warning p-4 text-center bg-info rounded">بچه ها : داستان زیر را بخوانند و از روی کلمات  آن دو بار با معنی بنویسند..  </div> */}
     {/* <div className="h5 m-5 border border-3 border-warning p-4 text-center bg-info rounded">بچه ها : داستان زیر را بخوانند و از روی 5 جمله آن بنویسند..  </div> */}
         {/* <VideoCard
       title="قصه کوتاه امروز"
@@ -32,15 +33,15 @@ export default function Home() {
   // imageSrc={`${process.env.PUBLIC_URL}/images/assetWord/designPage/girlSitting.png`}
   colors={["#ff0000", "#00ccff", "#ffaa00", "#8800ff","#10902eff", "#ff9500", "#ffcc00", "#34c759", "#0c09b7ff", "#5856d6", "#a0364aff"]}
 /> */}
-        <StoryPage3 groupKey="alefba" wantedTitle="کِیفِ مَدرِسِه"/>
+        {/* <StoryPage3 groupKey="alefba" wantedTitle="کِیفِ مَدرِسِه"/> */}
           {/* <SentenceBuilder/> */}
-        <WordGameWithCategories initialCategory="وسایل مدرسه" /> 
+        {/* <WordGameWithCategories initialCategory="وسایل مدرسه" />  */}
         {/* <StoryPage3 groupKey="alefba" wantedTitle="زَنبورِ کوچَک و باغِ پُرگُل"/> */}
         {/* <SentenceObject/> */}
         {/* <ChooseWords/> */}
         {/* <MatchGame titleGame=""/> */}
         {/* <StoryPage3 groupKey="kalemat" wantedTitle="گُلِ آفتاب‌گَردانِ میا"/> */}
-        {/* <TypingImageText srcImg="https://media.istockphoto.com/id/1204470108/vector/cute-kids-reading-book-and-librarian-in-library-flat-cartoon-illustration.jpg?s=612x612&w=0&k=20&c=NnfqdhoAmTMeNTBD6cvWHwc5pf8K3W-aCrbSOj9Plag="/> */}
+        <TypingImageText srcImg="https://media.istockphoto.com/id/1204470108/vector/cute-kids-reading-book-and-librarian-in-library-flat-cartoon-illustration.jpg?s=612x612&w=0&k=20&c=NnfqdhoAmTMeNTBD6cvWHwc5pf8K3W-aCrbSOj9Plag="/>
 
           {/* <StoryPage3 groupKey="kalemat" wantedTitle="دُکمه‌ی گُمشُده"/> */}
         {/* <DragDropQuiz/> */}
@@ -55,9 +56,9 @@ export default function Home() {
     {/* <VideoWithCaptions/> */}
 {/* ****************************************************** */}
     <div>
-      <h5 className="bg-warning p-4 m-4 rounded text-center">برای احسان : داستان زیر را بخواند و از روی کلمات داستان  دو بار را بنویسد.  </h5>
+      {/* <h5 className="bg-warning p-4 m-4 rounded text-center">برای احسان : داستان زیر را بخواند و از روی کلمات داستان  دو بار را بنویسد.  </h5> */}
       {/* <h5 className="bg-warning p-4 m-4 rounded text-center">برای احسان : جملات زیر را مرتب کند و از روی 5 جمله درست آن بنویسد.  </h5> */}
-        <StoryPage3 groupKey="kalemat" wantedTitle="اَوَّلین مُسابِقِه‌یِ طاها"/>
+        <StoryPage3 groupKey="kalemat" wantedTitle="یِک بویِ خُوشمَزه"/>
           {/* <SortableSentencesApp/> */}
           {/* <StoryPage3 groupKey="dastan" wantedTitle="قورباغه آوازخوان"/> */}
           {/* <SentenceBuilder/> */}
